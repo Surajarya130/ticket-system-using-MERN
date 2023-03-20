36c39ffb9334bc3d6e211fe20017c42d521fde4f
surajarya130 <surajarya130@gmail.com>
2023-03-20 14:58:10 +0530

    #2 Adding Routes and Controller to routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log("Server is there");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const userRouter = require("./routes/userRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Routes
+app.use("/api/users", userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`);
+});
